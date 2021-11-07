a813abdfb51049907c61c28315eb30f58291653d
atharv-ray1 <92596236+atharv-ray1@users.noreply.github.com>
2021-11-07 21:13:09 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCM69yeXy54loJw4GNNTwOB30htljK_L44",
+    authDomain: "project-94-bb860.firebaseapp.com",
+    databaseURL: "https://project-94-bb860-default-rtdb.firebaseio.com",
+    projectId: "project-94-bb860",
+    storageBucket: "project-94-bb860.appspot.com",
+    messagingSenderId: "643685454752",
+    appId: "1:643685454752:web:409a44c556844cf74c4ac5",
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
